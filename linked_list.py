6c70f950d1287434a6fdce25c9f98cf706ac62ac
LuisRemote <ortizangeles.mx@gmail.com>
2022-06-28 17:16:18 -0500

    added insert & search to linked list

--- linked_list.py
+++ linked_list.py
@@ -37,16 +37,78 @@ class LinkedList:
         
         return count
 
-def add(self, data):
-    """
-    Adds new Node containing data at head of the list
-    Takes O(1) time
-    """
+    def add(self, data):
+        """
+        Adds new Node containing data at head of the list
+        Takes O(1) time
+        """
 
-    new_node = Node(data)
-    new_node.next_node = self.head
-    self.head = new_node
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
 
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Return the node or 'None' if not found
+
+        Takes O(n) time (linear time)
+        """
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the node at the
+        insertion point takes O(n) time.
+
+        Takes overall O(n) time (linear time)
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = node.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def __repr__(self):
+        """
+        Return a string representation of the list
+        Takes O(n) time
+        """
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Trail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '-> '.join(nodes)
 
 
 
